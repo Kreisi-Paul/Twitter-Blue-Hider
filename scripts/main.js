559f424322b2f3198879bc5fd34cc55cd7c16cfc
Kreisi-Paul <111511210+Kreisi-Paul@users.noreply.github.com>
2023-05-21 01:34:16 +0200

    fixed #4 by removing the listener entirely

--- scripts/main.js
+++ scripts/main.js
@@ -10,32 +10,30 @@ if(/\/status\//.test(location.pathname)) {
     originalPost = location.pathname.split("/")[1];
 }
 
-const observer = new MutationObserver(domChanged);
-observer.observe(document.body, {childList: true, subtree: true});
 
-function domChanged() {
-    if(!domChangeTimeout) {
-        domChangeTimeout = true;
-        setTimeout(() => {domChangeTimeout = false}, 500); //only refreshes every 500ms
-        //console.log("DOM changed");
 
-        let twtBlueTweets = document.querySelectorAll("span:has(svg[data-testid=icon-verified] > g > path:not([fill]))");//Lists all hidden tweets
-        console.log(`detected ${twtBlueTweets.length} elements.`)
-        //console.log(twtBlueTweets)
+const styleEl = document.createElement("style");//create StyleElement to inject
+let styleStr = new String();//StyleElement content
 
-        chrome.storage.sync.get(["whitelist"]).then((result) => {//gets the whitelist...
-            whitelist = result.whitelist;
-            whitelist.push(originalPost);//adds OP to whitelist temporarily
-        });
+chrome.storage.sync.get(["whitelist"]).then((result) => {//gets the whitelist...
+    whitelist = result.whitelist;
+    whitelist.push(originalPost);//adds OP to whitelist temporarily
+    console.log(whitelist)
 
-        for(let i=0, iLength=whitelist.length; i<iLength; i++) {//iterates through all whitelisted usernames
-            let usernameHits = document.querySelectorAll(`div[data-testid=cellInnerDiv]:has(a[href="/${whitelist[i]}"])`);
-
-            for(let j=0,jLength=usernameHits.length; j<jLength; j++) {//iterates through all tweets of whitelisted user
-                usernameHits[j].style.display = "initial"
-                //usernameHits[j].style.background = "initial" //for debug
-            }
+    for(let i=0, iLength=whitelist.length; i<iLength; i++) {//iterates through all whitelisted usernames
+        console.log(i)
+        
+        styleStr += `
+        div[data-testid=cellInnerDiv]:has(a[href="/${whitelist[i]}"]) {
+            display: initial !important;
+            background-color: initial !important;
         }
+        `;
     }
 
-}
+    console.log(styleStr)
+    styleEl.innerText = styleStr;
+    document.head.appendChild(styleEl);//inject StyleElement
+});
+
+
