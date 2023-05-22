4810d701791a0ad7b843d68dedf0353d4baad864
Kreisi-Paul <111511210+Kreisi-Paul@users.noreply.github.com>
2023-05-22 02:25:27 +0200

    backend for "hide Tweets in Homefeed" option added

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,77 @@
 let domChangeTimeout = false;
 let whitelist = new Array();
 let originalPost = new String();
+const styleEl = document.createElement("style");//create StyleElement to inject
+let styleStr =`
+a[aria-label="Verified Organizations"], a[aria-label="Twitter Blue"] {
+    /*hides twitter blue ads*/
+    display: none;
+}
+svg[data-testid=icon-verified] {
+    /*keep this for whitelisting*/
+    color: rgb(244, 33, 46);
+}
+`;//StyleElement content, adds basic filters
 
 
-if(location.pathname == "/home")
-    console.log("home")
+if(location.pathname == "/home") {
+    chrome.storage.sync.get(["hideHome"]).then((result) => {
+        console.log(result)
+        if(result === true)
+            hideContent();//applies blocking filters
+        else
+            applyFilters();//applies basic filters
 
-if(/\/status\//.test(location.pathname)) {
-    originalPost = location.pathname.split("/")[1];
+        if(typeof result != Boolean)
+            chrome.storage.sync.set({"hideHome":false});//sets `hideHome` to the default value upon first init
+    });
+}
+else
+    hideContent();
+
+
+function hideContent() {
+
+    styleStr += `
+    div[data-testid=cellInnerDiv]:has(span > svg[data-testid=icon-verified] > g > path:not([fill])) {
+        /*hides articles that contain a verified Icon*/
+        /*background-color: darkred;*/
+        display: none;
+    }
+    div[data-testid=cellInnerDiv]:has(span > svg[data-testid=icon-verified] > g > path:not([fill])) + div:not(:has(article)) {
+        /*hides response dropdowns*/
+        /*background-color: greenyellow;*/
+        display: none;
+    }
+    `;//adds universal blocking filters
+
+    if(/\/status\//.test(location.pathname)) {
+        originalPost = location.pathname.split("/")[1];
+    }
+    
+    
+    chrome.storage.sync.get(["whitelist"]).then((result) => {//gets the whitelist...
+        whitelist = result.whitelist;
+        whitelist.push(originalPost);//adds OP to whitelist temporarily
+        console.log(whitelist)
+    
+        for(let i=0, iLength=whitelist.length; i<iLength; i++) {//iterates through all whitelisted usernames
+            console.log(i)
+            
+            styleStr += `
+            div[data-testid=cellInnerDiv]:has(a[href="/${whitelist[i]}"]) {
+                display: initial !important;
+                background-color: initial !important;
+            }
+            `;
+        }
+        applyFilters();
+    });
+
 }
 
-
-
-const styleEl = document.createElement("style");//create StyleElement to inject
-let styleStr = new String();//StyleElement content
-
-chrome.storage.sync.get(["whitelist"]).then((result) => {//gets the whitelist...
-    whitelist = result.whitelist;
-    whitelist.push(originalPost);//adds OP to whitelist temporarily
-    console.log(whitelist)
-
-    for(let i=0, iLength=whitelist.length; i<iLength; i++) {//iterates through all whitelisted usernames
-        console.log(i)
-        
-        styleStr += `
-        div[data-testid=cellInnerDiv]:has(a[href="/${whitelist[i]}"]) {
-            display: initial !important;
-            background-color: initial !important;
-        }
-        `;
-    }
-
+function applyFilters() {
     console.log(styleStr)
     styleEl.innerText = styleStr;
     document.head.appendChild(styleEl);//inject StyleElement
-});
-
-
+}
